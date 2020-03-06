5fd21feb04a2b32df77a04680b17033650734af1
EdenVisualWorks <61868125+EdenVisualWorks@users.noreply.github.com>
2020-03-06 17:44:34 +0800

    Add files via upload

---
+++ nomScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class nomScript : MonoBehaviour
+{
+
+    public bool valueEntered = false;
+    public bool valuePresent = false;
+
+    private string newValue;
+
+    
+    public InputField QuantityField;
+    
+
+    public void valueCheck()
+    {
+        valueEntered = true;
+    }
+
+    public void SaveValue(string)
+    {
+        if (valueEntered == true)   
+        {
+            valuePresent = true;
+            QuantityField.text = PlayerPrefs.SetString("saveValue");            
+        }
+    }
+
+    public void LoadValue()
+    {
+        if (valuePresent == true)
+        {
+            QuantityField.text = PlayerPrefs.GetString("saveValue");
+            Debug.Log("Values Loaded!");
+        }
+    }
+
+    
+
+    public void Update()
+    {
+        Debug.Log(valueEntered + "yey");
+        Debug.Log(valueEntered + "woo");
+    }
+
+    
+    
+}
